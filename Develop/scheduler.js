5534164e205f2e6ea4fb3b1e6e45f5ec3f802663
Dylan Dewey <dylandewey41@gmail.com>
2020-06-24 16:28:04 -0600

    made a function to put user input into an object

--- Develop/scheduler.js
+++ Develop/scheduler.js
@@ -1,14 +1,32 @@
+//Variables
 const date = $('#currentDay');
+const timeBlock = $('time-block');
+let todoItems = [];
 
-
-//Gets time form momentjs
-$(document).ready(function () {
+//Gets time from momentjs
+$(document).ready(function() {
     // sets date text in header
     const todaysDate = moment().format('LLLL');
-    let dateHeader = (date) 
-    dateHeader.text(todaysDate);
+    let currentDate = (date) 
+    currentDate.text(todaysDate);
     console.log(todaysDate);
 })
+
+//Setup and array of objects for user input in each time slot
+function schedule() {
+    $timeBlock.each(function() {
+        let thisBlock = $(this);
+        let thisBlockHr = parseInt (thisBlock.attr('data-hour'));
+        let todo = {
+            hour: thisBlockHr,
+            text: '',
+        }
+        todoItems.push(todo);
+        console.log(todoItems);
+    });
+    localStorage.setItem('todos', JSON.stringify(todoItems));
+}
+
     
 
     
